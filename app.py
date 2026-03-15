42c01ebe036c298553978f9295f567cbf41f7d7f
José Manuel Cabrera Rojas <josemanuelcabrera2011@gmail.com>
2026-03-15 12:32:31 -0600

    fix: simple tick missing

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.header('Lanzar una moneda')
 
 number_of_trails = st.slider('¿Número de intentos?', 1,1000,10)
-start_button = st.button('Ejecutar)
+start_button = st.button('Ejecutar')
 
 if start_button:
     st.write(f'Experimento con {number_of_trails} intentos en curso.')
