348156f89b71eebc1baa6778e5f7fb0b8e13dae6
José Manuel Cabrera Rojas <josemanuelcabrera2011@gmail.com>
2026-03-15 12:29:54 -0600

    add:interface to control the game to drop coin simulation

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
 import streamlit as st
+
 st.header('Lanzar una moneda')
-st.write('Esta aplicación aún no es duncional. En construccion.')
+
+number_of_trails = st.slider('¿Número de intentos?', 1,1000,10)
+start_button = st.button('Ejecutar)
+
+if start_button:
+    st.write(f'Experimento con {number_of_trails} intentos en curso.')
+
+st.write('Esta aplicación aún no es funcional. EN construcción.')
