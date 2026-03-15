4d63fe0011bbb93ba99c35ae94c661c14e8af4c2
José Manuel Cabrera Rojas <josemanuelcabrera2011@gmail.com>
2026-03-15 12:52:51 -0600

    add: buton funtionality calling toss_conin fucntion

--- app.py
+++ app.py
@@ -28,4 +28,5 @@ number_of_trails = st.slider('¿Número de intentos?', 1,1000,10)
 start_button = st.button('Ejecutar')
 
 if start_button:
-    st.write(f'Experimento con {number_of_trails} intentos en curso.')
+    st.write(f'Experimento con {number_of_trails} intentos en curso.'
+    mean = toss_coin(number_of_trails))
